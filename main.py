7f4b2cd75ebb5140cc5d03d8705af65bac8eef2e
Yaroslaw <bruce.levitangens@gmail.com>
2019-05-31 21:20:25 +0500

    Update main.py
    add errors

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 def fac(x):
     def factorial(n):
+        n = int(n)
         if n == 0:
             return 1
         return factorial(n - 1) * n
 
-    return factorial(int(x)) * (int(x) + 1) ** (x - int(x))
+    if x < 0:
+        return "Error: number < 0"
+    elif (x >= 0) and (x <= 1):
+        return 1
+    elif int(x) == x:
+        if x >= 997:
+            return "Error: number >= 997"
+        return factorial(x)
+    else:
+        if x >= 170.62421348621231:
+            return "Error: factorial goes beyond float type"
+        return factorial(int(x)) * (int(x) + 1) ** (x - int(x))
