57933bcafb2ba0e84641c4870ead7729a407a80a
Yaroslaw <bruce.levitangens@gmail.com>
2019-05-31 21:59:44 +0500

    Add new func and comment
    Update list: 
    * Added comments explaining the methods 
    * Added multi factorial (Won't forget to add function’s description in readme) 
    * Added super factorial (Won't forget to add function’s description in readme)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def fac(x):
-    def factorial(n):
-        n = int(n)
-        if n == 0:
-            return 1
-        return factorial(n - 1) * n
+# Private method for finding factorial
+def factorial(n):
+    n = int(n)
+    if n == 0:
+        return 1
+    return factorial(n - 1) * n
 
+# Factorial
+def fac(x):
     if x < 0:
         return "Error: number < 0"
     elif (x >= 0) and (x <= 1):
@@ -16,6 +18,24 @@ def fac(x):
             return "Error: large number"
     else:
         try:
+            # Search factorials fractional numbers
             return factorial(int(x)) * (int(x) + 1) ** (x - int(x))
         except OverflowError:
             return "Error: factorial goes beyond float type"
+
+# Multi factorial
+def multf(x, m):
+    out = x
+    i = 0
+
+    while i < int(m):
+        out = fac(out)
+        i += 1
+    return out
+
+# Super factorial
+def supf(m):
+    out = 1
+    for x in range(2, int(m)+1):
+        out *= fac(x)
+    return out
